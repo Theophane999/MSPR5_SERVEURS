13e02c6acaa842e3572895cf36c7ec493aea8e83
Theophane999 <domestico.theophane@gmail.com>
2026-07-03 00:07:54 +0200

    test(child): isolate InfoController test from datasource

--- services/backend-child/src/test/java/com/futurekawa/child/InfoControllerTest.java
+++ services/backend-child/src/test/java/com/futurekawa/child/InfoControllerTest.java
@@ -6,13 +6,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(InfoController.class)
 @TestPropertySource(properties = {"app.country=Brazil", "app.mqtt.enabled=false"})
 class InfoControllerTest {
 
